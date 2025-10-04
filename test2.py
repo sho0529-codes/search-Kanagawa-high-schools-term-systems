566e3b073a9e14d5d84d02d016c235c7ce48b0dc
sho0529-codes <sho0529.codes@gmail.com>
2025-10-04 15:04:56 +0900

    urlの出力まで

--- test2.py
+++ test2.py
@@ -4,7 +4,13 @@ from urllib.parse import quote
 # 「https://ja.wikipedia.org/wiki/高校名」みたいになってる
 # https://ja.wikipedia.org/wiki/
 
-school_name = pd.read_csv("output/school_name.csv", encoding="utf-8")
-for name in school_name["学校名"]:
-    print(f"https://ja.wikipedia.org/wiki/{name}") # urllib.parse.quote()でへんかんするらしい
-    print(f"https://ja.wikipedia.org/wiki/{quote(name)}") # なんかある程度はwikipediaが補正してくれてる？
+input_file = "output/school_name.csv"
+output_file= "output/school_url.csv"
+
+school_name = pd.read_csv(input_file, encoding="utf-8")
+with open(output_file, "w", encoding="utf-8") as f:
+    for name in school_name["学校名"]:
+        # print(f"https://ja.wikipedia.org/wiki/{name}") # urllib.parse.quote()でへんかんするらしい
+        # print(f"https://ja.wikipedia.org/wiki/{quote(name)}") # なんかある程度はwikipediaが補正してくれてる？
+        f.write(f"{name}：https://ja.wikipedia.org/wiki/{quote(name)}\n")
+print("finish")
---
+++ test3.py
@@ -0,0 +1,17 @@
+# これ（https://qiita.com/Octoparse_Japan/items/902f336d1b328eb38dbf）参考にする
+
+import requests
+from bs4 import BeautifulSoup
+
+# 解析するWebサイトのURL
+# url = "https://example.com"  # 解析したいURL
+url = "https://ja.wikipedia.org/wiki/%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C%E7%AB%8B%E9%B6%B4%E8%A6%8B%E9%AB%98%E7%AD%89%E5%AD%A6%E6%A0%A1"
+
+# HTMLを取得
+response = requests.get(url)
+
+# BeautifulSoupで解析
+soup = BeautifulSoup(response.content, "html.parser")
+
+# HTML全体を出力
+print(soup.prettify())
